d7c677ee74df8dec913619496b86fcd8eca3d4ea
Timothy Beh <timbeh4@gmail.com>
2024-09-22 22:49:23 +1000

    finalise week 9

---
+++ vega_lite_vis.js
@@ -0,0 +1,4 @@
+var vg_1 = "Homework_9_map.vg.json";
+ vegaEmbed("#map_symbol", vg_1).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+ }).catch(console.error);
