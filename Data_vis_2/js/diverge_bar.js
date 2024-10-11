82d62f2bc59b6f5faa09184b35fb54bef25f50de
Timothy Beh <timbeh4@gmail.com>
2024-10-11 14:52:01 +1100

    draft

---
+++ Data_vis_2/js/diverge_bar.js
@@ -0,0 +1,4 @@
+var vg_1 = "js/vega_files/diverge_bar_chart.vg.json";
+ vegaEmbed("#vis3", vg_1).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+ }).catch(console.error);
---
+++ Data_vis_2/js/reason_combine.js
@@ -0,0 +1,4 @@
+var vg_1 = "js/vega_files/reason_combine.vg.json";
+ vegaEmbed("#vis5", vg_1).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+ }).catch(console.error);
